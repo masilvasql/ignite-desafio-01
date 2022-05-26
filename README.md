d25d3db58ba802e8d9ec9fd137e463c4a0628c9e
Marcelo Abrão <masilvasql@gmail.com>
2022-05-25 21:15:20 -0300

    finalização dos testes

---
+++ README.md
@@ -0,0 +1,6 @@
+[x] POST/users
+[x] GET/todos
+[x] POST/todos
+[x] PUT/todos/:id
+[x] PATCH /todos/:id/done
+[x] DELETE /todos/:id
---
+++ src/index.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const app = express();
+const {v4:uuidV4} = require('uuid')
+
+app.use(express.json())
+
+const users = [];
+
+function checkIfExisitsUserAccount(request, response, next){
+
+    const {username} = request.headers;
+
+    const user = users.find(user => user.username === username)
+
+    if(!user){
+        return response.status(400).json({error:"user not found!"})
+    }
+
+    request.user = user;
+
+    next()
+}
+
+function findTodo(todos, id){
+    return todos.find(todo=> todo.id === id );
+}
+
+function updateTodo(todos, id, title, deadline){
+    const todo = findTodo(todos, id)
+    todo.title = title;
+    todo.deadline = new Date(deadline);
+}
+
+function deleteTodo(todos, id){
+    const todo = findTodo(todos, id)
+    todos.splice(todo, 1);
+}
+
+function doneTodo(todos, id){
+    const todo = todos.find(todo=> todo.id === id );
+    todo.done = true;
+}
+
+
+app.post("/users", (request, response)=>{
+    const {name, username} = request.body;
+    const todos = [];
+
+    let json = {
+        name,
+        username,
+        id:uuidV4(),
+        todos
+    }
+
+    users.push(json);
+
+    return response.status(201).json(json)
+})
+
+app.post("/todos", checkIfExisitsUserAccount, (request, response)=>{
+    const {title, deadline} = request.body;
+    const {user} = request;
+    user.todos.push({
+        id:uuidV4(),
+        title , 
+        done:false,
+        deadline:new Date(deadline),
+        created_at: new Date()
+    })
+
+    return response.status(201).json(user.todos);
+
+})
+
+app.get("/todos", checkIfExisitsUserAccount, (request, response)=>{
+    const {user} = request;
+    return response.json(user.todos)
+})
+
+app.put("/todos/:id",checkIfExisitsUserAccount ,(request, response)=>{
+    const {user} = request;
+    const {id} = request.params;
+    const {title, deadline} = request.body;
+
+    updateTodo(user.todos, id, title, deadline);
+
+    response.status(201).json(user.todos)
+
+})
+
+app.patch("/todos/:id/done", checkIfExisitsUserAccount ,(request, response)=>{
+    const {user} = request;
+    const {id} = request.params;
+ 
+    doneTodo(user.todos,id);
+
+    response.status(201).json(user.todos)
+
+})
+
+app.delete("/todos/:id", checkIfExisitsUserAccount, (request, response)=>{
+    const {user} = request;
+    const {id} = request.params;
+
+    deleteTodo(user.todos, id);
+
+    return response.status(200).json(user.todos)
+})
+
+app.listen(3333, ()=>{
+    console.log(`Server is running`)
+})
